4a439065d77fc27603201f6b9aa9ff99ced413ca
Ally0199 <alondra.ramirez1079@alumnos.udg.mx>
2022-12-23 18:39:41 -0600

    eventosPlataformas
    Co-authored-by: oswaldomercado26 <oswaldomercado26@users.noreply.github.com>
    Co-authored-by: hekamiahlunaq <hekamiahlunaq@users.noreply.github.com>

--- frontend/src/slider.js
+++ frontend/src/slider.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import banner from './banner.jpg'
-import banner1 from './banner1.jpg'
-import banner2 from './banner2.jpg'
+import netflix from './logosPlataformas/netflix.jpg'
+import disney from './logosPlataformas/disneyplus.jpg'
+import hbo from './logosPlataformas/hbomax.png'
+import paramount from './logosPlataformas/paramount.jpg'
+import prime from './logosPlataformas/primevideo.png'
+import starplus from './logosPlataformas/starplus.png'
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -41,10 +45,28 @@ useEffect( () => {
   sizeofscreen();
   
 },[]);
+//Eventos de plataformas
+const eventoNetflix = document.querySelector('#netflix')
+eventoNetflix.addEventListener('click',() => console.log('Diste Click en netflix'))
+
+const eventoDisney = document.querySelector('#disney')
+eventoDisney.addEventListener('click',() => console.log('Diste Click en disney'))
+
+const eventoHBO = document.querySelector('#hbo')
+eventoHBO.addEventListener('click',() => console.log('Diste Click en hbo'))
+
+const eventoParamount = document.querySelector('#paramount')
+eventoParamount.addEventListener('click',() => console.log('Diste Click en paramount'))
+
+const eventoPrime = document.querySelector('#prime')
+eventoPrime.addEventListener('click',() => console.log('Diste Click en prime'))
+
+const eventoStarplus = document.querySelector('#starplus')
+eventoStarplus.addEventListener('click',() => console.log('Diste Click en starplus'))
+
 if(SlideNumber>0){
   return (
 
-    
     <>
     
     <h1 className="Featured"> Presentado</h1>
@@ -59,27 +81,20 @@ if(SlideNumber>0){
           clickable: true,
         }}
         
-        
-
         navigation={true}
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><img src={banner}></img></SwiperSlide>
-        <SwiperSlide><img src={banner1}></img></SwiperSlide>
-        <SwiperSlide><img src={banner2}></img></SwiperSlide>
-        <SwiperSlide><img src={banner}></img></SwiperSlide>
-        <SwiperSlide><img src={banner1}></img></SwiperSlide>
-        <SwiperSlide><img src={banner2}></img></SwiperSlide>
-        <SwiperSlide><img src={banner}></img></SwiperSlide>
-        <SwiperSlide><img src={banner1}></img></SwiperSlide>
-        <SwiperSlide><img src={banner2}></img></SwiperSlide>
-        <SwiperSlide><img src={banner}></img></SwiperSlide>
-        <SwiperSlide><img src={banner1}></img></SwiperSlide>
-        <SwiperSlide><img src={banner2}></img></SwiperSlide>
+        <SwiperSlide><img id="netflix" src={netflix}></img></SwiperSlide>
+        <SwiperSlide><img id="disney" src={disney}></img></SwiperSlide>
+        <SwiperSlide><img id="hbo" src={hbo}></img></SwiperSlide>
+        <SwiperSlide><img id="paramount" src={paramount}></img></SwiperSlide>
+        <SwiperSlide><img id="prime" src={prime}></img></SwiperSlide>
+        <SwiperSlide><img id="starplus" src={starplus}></img></SwiperSlide>
       </Swiper>
     </>
   );
+  
       }
       else{
 
