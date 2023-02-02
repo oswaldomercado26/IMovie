a0f53abeb3ed88f1cf397abc830424c4d2a6fab7
oswaldomercado26 <79757745+oswaldomercado26@users.noreply.github.com>
2023-02-02 16:50:37 -0600

    Update Top3.jsx

--- frontend/src/components/common/Top3.jsx
+++ frontend/src/components/common/Top3.jsx
@@ -58,3 +58,39 @@ const Top3 = () => {
   );
 };
 export default Top3;
+
+
+
+  <Container>
+          <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Netflix</th>
+          <th>HBO</th>
+          <th>Disney</th>
+          <th>Prime Video</th>
+          <th>Paramount+</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    </Container>
