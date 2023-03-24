0b29124cfa0c8b95e6c401559b8c1121e0afa6b9
oswaldomercado26 <79757745+oswaldomercado26@users.noreply.github.com>
2023-03-23 21:56:01 -0600

    Update firebaseStorage.js

--- server/config/firebaseStorage.js
+++ server/config/firebaseStorage.js
@@ -9,7 +9,8 @@ initializeApp({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.PROJECT_ID,
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY ?    ? JSON.parse(process.env.PRIVATE_KEY)
+    private_key: process.env.PRIVATE_KEY 
+          ? JSON.parse(process.env.PRIVATE_KEY)
           : undefined,
     client_email: process.env.CLIENT_EMAIL,
     client_id: process.env.CLIENT_ID,
